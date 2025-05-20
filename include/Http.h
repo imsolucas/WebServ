c299de8acf715b4469008f37979e64279b6d005a
JunYut <tanjunyu8888@gmail.com>
2025-05-20 15:16:39 +0800

    reconsidering choices ...

--- include/Http.h
+++ include/Http.h
@@ -2,11 +2,37 @@
 
 # include <iostream>
 # include <map>
+# include <vector>
+
+typedef char * ByteStream;
 
 enum MessageType
 {
 	REQUEST,
 	RESPONSE
+};
+
+enum HttpMethod
+{
+	GET,
+	POST,
+	DELETE
+};
+
+enum HttpHeader
+{
+	HOST,
+	CONTENT_LENTGTH,
+	CONTENT_TYPE,
+	ACCEPT,
+	AUTHORIZATION,
+	COOKIE,
+	CONNECTION,
+	DATE,
+	SERVER,
+	LOCATION,
+	ALLOW,
+	SET_COOKIE
 };
 
 enum StatusCode
@@ -23,15 +49,31 @@ enum StatusCode
 	BAD_GATEWAY = 502
 };
 
-struct HttpHeader
-{
-	std::map<std::string, std::string> fields;
-};
-
 struct HttpMessage
 {
 	MessageType	type;
 	std::string	startLine;
-	HttpHeader	header;
+	std::map<std::string, std::string> headers;
 	std::string	body;
 };
+
+struct HttpRequest
+{
+	HttpMethod	method;
+	std::string	URI;
+	std::string	httpVersion;
+	std::map<HttpHeader, std::string> headers;
+	std::string	body;
+};
+
+struct HttpResponse
+{
+	std::string	httpVersion;
+	StatusCode	status;
+	std::string	reasonPhrase;
+	std::map<HttpHeader, std::string> headers;
+	std::string	body;
+};
+
+ByteStream	serialize(HttpResponse response);
+HttpRequest	deserialize(ByteStream request);
