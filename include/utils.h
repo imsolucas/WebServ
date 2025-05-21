1260add0e242ecf2ecb5a47132bc949a141f093b
JunYut <tanjunyu8888@gmail.com>
2025-05-21 14:04:33 +0800

    feat: added utils module

---
+++ include/utils.h
@@ -0,0 +1,9 @@
+# pragma once
+
+# include <string>
+# include <vector>
+
+namespace utils
+{
+	std::vector<std::string> split(const std::string &str, char delimiter);
+}
---
+++ src/utils/string.cpp
@@ -0,0 +1,20 @@
+# include <sstream>
+
+# include "utils.h"
+
+using std::string;
+using std::istringstream;
+using std::vector;
+using std::getline;
+
+vector<string> utils::split(const string &str, char delimiter)
+{
+	istringstream	iss(str);
+	string			token;
+	vector<string>	v;
+
+	while (getline(iss, token, delimiter))
+		v.push_back(token);
+		
+	return v;
+}
