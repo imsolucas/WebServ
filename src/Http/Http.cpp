3063f22af86019f265b56aaa0998fe0c6cb2e4ab
JunYut <tanjunyu8888@gmail.com>
2025-05-21 11:46:15 +0800

    implemented decode()

--- src/Http/Http.cpp
+++ src/Http/Http.cpp
@@ -1,1 +1,66 @@
+# include <iostream>
+# include <sstream>
+
 # include "Http.h"
+
+using std::cout;
+using std::getline;
+using std::string;
+using std::istringstream;
+
+static HttpRequest parse(HttpMessage message);
+static HttpMessage decode(ByteStream stream);
+
+ByteStream serialize(HttpResponse response)
+{
+	(void)response;
+	return NULL;
+}
+
+// assumes the byte stream is a valid HTTP request
+HttpRequest deserialize(ByteStream stream)
+{
+	HttpMessage msg;
+	HttpRequest req;
+
+	msg = decode(stream);
+	cout << msg.startLine;
+	cout << msg.headers;
+	cout << msg.body;
+	req = parse(msg);
+
+	return req;
+}
+
+HttpRequest parse(HttpMessage message)
+{
+	(void)message;
+	HttpRequest req;
+	return (req);
+}
+
+HttpMessage decode(ByteStream stream)
+{
+	istringstream iss(stream);
+	string line;
+	HttpMessage msg;
+
+	// Read start line
+	getline(iss, msg.startLine);
+
+	// Read headers until an empty line
+	while (getline(iss, line))
+	{
+		if (!line.empty() && line[line.length() - 1] == '\r')
+			line.erase(line.length() - 1);
+		if (line.empty())
+			break;
+		msg.headers += (line + "\n");
+	}
+
+	// Read the body
+	while (getline(iss, line))
+		msg.body += (line + "\n");
+
+	return msg;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,64 @@
 # include <iostream>
 
 # include "colors.h"
+# include "Http.h"
 # include "WebServer.hpp"
 
+using std::map;
+using std::string;
+using std::cout;
 using std::cerr;
 
 int main(int argc, char *argv[])
 {
+	(void)argv;
 	if (argc != 2)
 	{
 		cerr << RED + "Usage: ./webserv [configuration file]\n";
 		return 1;
 	}
 
-	WebServer wb(argv[1]);
-	wb.run();
+	// GET request
+	{
+		ByteStream stream =
+		"GET /index.html HTTP/1.1\r\n"
+		"Host: example.com\r\n"
+		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n"
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9\r\n"
+		"Connection: keep-alive\r\n"
+		"\r\n";
+
+		HttpRequest request = deserialize(stream);
+
+		cout << request.method << '\n';
+		cout << request.requestTarget << '\n';
+		cout << request.protocol << '\n';
+		for (map<string, string>::const_iterator it = request.requestHeaders.begin(); it != request.requestHeaders.end(); ++it)
+			cout << (*it).first + ": " + (*it).second + '\n';
+		cout << request.body << '\n';
+	}
+	// POST request
+	{
+		ByteStream stream =
+			"POST /api/v1/resource HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n"
+			"Content-Type: application/json\r\n"
+			"Content-Length: 34\r\n"
+			"Connection: keep-alive\r\n"
+			"\r\n"
+			"{\n"
+			"    \"name\": \"John Doe\",\n"
+			"    \"age\": 30\n"
+			"}";
+
+		HttpRequest request = deserialize(stream);
+
+		cout << request.method << '\n';
+		cout << request.requestTarget << '\n';
+		cout << request.protocol << '\n';
+		for (map<string, string>::const_iterator it = request.requestHeaders.begin(); it != request.requestHeaders.end(); ++it)
+			cout << (*it).first + ": " + (*it).second + '\n';
+		cout << request.body << '\n';
+	}
 }
