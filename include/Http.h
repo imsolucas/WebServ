1c66f58d419a452a581856ee1ac4b83c31560750
JunYut <tanjunyu8888@gmail.com>
2025-05-21 09:37:56 +0800

    changed HttpHeaders into const strings

--- include/Http.h
+++ include/Http.h
@@ -19,22 +19,6 @@ enum HttpMethod
 	DELETE
 };
 
-enum HttpHeader
-{
-	HOST,
-	CONTENT_LENTGTH,
-	CONTENT_TYPE,
-	ACCEPT,
-	AUTHORIZATION,
-	COOKIE,
-	CONNECTION,
-	DATE,
-	SERVER,
-	LOCATION,
-	ALLOW,
-	SET_COOKIE
-};
-
 enum StatusCode
 {
 	OK = 200,
@@ -49,20 +33,27 @@ enum StatusCode
 	BAD_GATEWAY = 502
 };
 
-struct HttpMessage
-{
-	MessageType	type;
-	std::string	startLine;
-	std::map<std::string, std::string> headers;
-	std::string	body;
-};
+namespace HttpHeaders {
+	const std::string HOST = "Host";
+	const std::string CONTENT_LENGTH = "Content-Length";
+	const std::string CONTENT_TYPE = "Content-Type";
+	const std::string ACCEPT = "Accept";
+	const std::string AUTHORIZATION = "Authorization";
+	const std::string COOKIE = "Cookie";
+	const std::string CONNECTION = "Connection";
+	const std::string DATE = "Date";
+	const std::string SERVER = "Server";
+	const std::string LOCATION = "Location";
+	const std::string ALLOW = "Allow";
+	const std::string SET_COOKIE = "Set-Cookie";
+}
 
 struct HttpRequest
 {
 	HttpMethod	method;
 	std::string	URI;
 	std::string	httpVersion;
-	std::map<HttpHeader, std::string> headers;
+	std::map<std::string, std::string> headers;
 	std::string	body;
 };
 
@@ -71,9 +62,9 @@ struct HttpResponse
 	std::string	httpVersion;
 	StatusCode	status;
 	std::string	reasonPhrase;
-	std::map<HttpHeader, std::string> headers;
+	std::map<std::string, std::string> headers;
 	std::string	body;
 };
 
 ByteStream	serialize(HttpResponse response);
-HttpRequest	deserialize(ByteStream request);
+HttpRequest	deserialize(ByteStream stream);
