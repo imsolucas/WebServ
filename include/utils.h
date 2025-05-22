346b27015d8d1b10edf6a8a5b69de26b633d9b58
JunYut <tanjunyu8888@gmail.com>
2025-05-22 15:05:11 +0800

    refactor: changed utils{} from namespace -> struct

--- include/utils.h
+++
@@ -1,9 +0,0 @@
-# pragma once
-
-# include <string>
-# include <vector>
-
-namespace utils
-{
-	std::vector<std::string> split(const std::string &str, char delimiter);
-}
---
+++ include/utils.hpp
@@ -0,0 +1,12 @@
+# pragma once
+
+# include <string>
+# include <vector>
+
+struct utils
+{
+	template <typename T>
+	static std::string toString(const T &any);
+	
+	static std::vector<std::string> split(const std::string &str, char delimiter);
+};
