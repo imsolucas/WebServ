ccadb4cbc89f916ced0609b2688c49e2f76f7be1
JunYut <tanjunyu8888@gmail.com>
2025-05-23 23:21:34 +0800

    feat: added macros for dividers

--- test/Test.hpp
+++ test/Test.hpp
@@ -2,6 +2,9 @@
 
 # include <iostream>
 
+# define DIVIDER_THICK "\n"_BG_BLUE"=============================="_RESET"\n"
+# define DIVIDER_THIN "\n"_BG_CYAN"------------------------------"_RESET"\n"
+
 template <typename Output>
 void assertEqual(const std::string &msg, const Output &actual, const Output &expected);
 
