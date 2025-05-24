d3e0d3ce5c0f6829dc92d7ca1c3672db5dda98b1
JunYut <tanjunyu8888@gmail.com>
2025-05-24 19:30:37 +0800

    feat: implemented class TestSuite{}

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,48 @@
 # include "test.hpp"
 
 using std::string;
+using std::vector;
 using std::cerr;
+
+TestSuite::TestSuite()
+{
+	cerr << DIVIDER_THICK_BG;
+	cerr << BOLD << "TEST SUITE" << RESET;
+	cerr << DIVIDER_THICK_BG;
+}
+
+void TestSuite::addTest(bool (*t)())
+{
+	_tests.push_back(t);
+}
+
+void TestSuite::run()
+{
+	bool result;
+
+	for (vector<bool (*)()>::iterator it = _tests.begin();
+		it != _tests.end(); ++it)
+	{
+		result = *it;
+		if (result == true) _passes += 1;
+		else _fails += 1;
+	}
+	printSummary(_tests.size(), _passes, _fails);
+}
 
 void printHeader(const string &msg)
 {
 	cerr << GREEN + msg + RESET;
+	cerr << DIVIDER_THICK;
+}
+
+void printSummary(unsigned int tests, unsigned int passes, unsigned int fails)
+{
+	cerr << DIVIDER_THICK;
+	cerr << BOLD << "Test Summary" << RESET << "\n";
+	cerr << DIVIDER_THIN;
+	cerr << "Total:   " << tests << " ";
+	cerr << "Passed:  " << GREEN << passes << RESET << " ";
+	cerr << "Failed:  " << RED << fails << RESET << "\n";
 	cerr << DIVIDER_THICK;
 }
--- test/test.hpp
+++ test/test.hpp
@@ -1,6 +1,7 @@
 # pragma once
 
 # include <iostream>
+# include <vector>
 
 # define DIVIDER_THICK_BG "\n"_BG_BLUE"=============================="_RESET"\n"
 # define DIVIDER_THIN_BG "\n"_BG_CYAN"------------------------------"_RESET"\n"
@@ -8,10 +9,25 @@
 # define DIVIDER_THICK "\n"_BLUE"=============================="_RESET"\n"
 # define DIVIDER_THIN "\n"_CYAN"------------------------------"_RESET"\n"
 
+class TestSuite
+{
+	public:
+		TestSuite();
+
+		void addTest(bool (*t)());
+		void run();
+
+	private:
+		std::vector<bool (*)()> _tests;
+		unsigned int _passes;
+		unsigned int _fails;
+};
+
 template <typename Output>
 void assertEqual(const std::string &msg, const Output &actual, const Output &expected);
 
 void printHeader(const std::string &msg);
+void printSummary(unsigned int tests, unsigned int passes, unsigned int fails);
 
 void test_http();
 
